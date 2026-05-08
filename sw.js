1e02fd57e90b551766fac4a058d3302f6132d55f
connectsilva <connectsilvaoficial@gmail.com>
2026-05-07 22:35:22 -0300

    Teste 2.2

--- sw.js
+++ sw.js
@@ -1,23 +1,19 @@
-const CACHE_NAME = 'capoeira-cache-v1';
-const assetsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.png'
+const CACHE_NAME = 'capoeira-v1';
+const ASSETS = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'icon-192.png' // Adicione aqui seus ícones e outros arquivos locais
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assetsToCache);
-    })
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
